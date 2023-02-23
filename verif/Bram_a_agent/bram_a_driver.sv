9da8b161e3a9bb1c47ec606ccb6b3dc64f34d3e5
mladenilic23 <67113935+mladenilic23@users.noreply.github.com>
2023-02-23 14:47:45 +0100

    Update bram_a_driver.sv

--- verif/Bram_a_agent/bram_a_driver.sv
+++ verif/Bram_a_agent/bram_a_driver.sv
@@ -19,38 +19,35 @@ class bram_a_driver extends uvm_driver #(bram_a_item);
       	if (!uvm_config_db#(virtual bram_a_if)::get(this, "*", "bram_a_if", vif))
       	`uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"})
    	endfunction : connect_phase
-   
-
-	// process item
-	task run_phase(uvm_phase phase);
-     
-      forever begin
-	      
+   	
+// process item
+		
+task run_phase(uvm_phase phase);   
+	
+	forever begin
+		
 		@(posedge vif.clock)begin	   
 	        
-	         address = vif.s_addr_bram_a;
+	 	address = vif.s_addr_bram_a;
 	         	    
-	         if(vif.s_en_bram_a)begin
+			if(vif.s_en_bram_a)begin
 		         
-		         if(vif.s_addr_bram_a < 4607)begin
+				if(vif.s_addr_bram_a < 1152)begin
 
-		            //seq_item_port.get_next_item(req);
-		            //req.address = address;
-		        	//seq_item_port.item_done();
+					seq_item_port.get_next_item(req);
+					req.address = address;
+					seq_item_port.item_done();
 
-		            seq_item_port.get_next_item(req);
-		            `uvm_info(get_type_name(), $sformatf("Driver sending...\n%s", req.sprint()), UVM_HIGH)
+					seq_item_port.get_next_item(req);
+					`uvm_info(get_type_name(), $sformatf("Driver sending...\n%s", req.sprint()), UVM_HIGH)
 		            
-		            vif.s_dout_bram_a = req.in_data;
-		            seq_item_port.item_done();
+					vif.s_dout_bram_a = req.in_data;
+					seq_item_port.item_done();
 		            		  		            		            
-		         end 	       
-			 end 
-	       
+				end 	       
+		 	end       
 		end 	    
-	      
-      end 
-      
-   	endtask : run_phase
+	end      
+endtask : run_phase
 
 endclass : bram_a_driver
